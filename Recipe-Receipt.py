0a0e827f8989720f14e943cf19cc2d874f2a00f4
Khang Nguyen <nnmkhang166@gmail.com>
2018-08-26 19:09:11 -0400

    Added another cooking website

--- Recipe-Receipt.py
+++ Recipe-Receipt.py
@@ -9,47 +9,66 @@ import selenium # used to "click" on the print button to open the print page
 # printed. 
 
 
-# will start with budget bytes since it is the recipe website i use the most. 
-# unsure how to parse multiple kinds of websites 
+# will start with budget bytes since it is the recipe website I use the most. 
+# unsure how to parse multiple kinds of websites.
+# will add allrecipes and sam the cooking guy to this list as well
 
-def getRecipe(url):
-    res = requests.get(url)
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text,"html.parser")
-    elems = soup.select("#recipe-notes > span.recipe-directions--print > a")#css selector 
-    print(soup.title)
-
-#def clean(str): # removes all the beginning tags 
 
     
 url = input("imput url")
-
 res = requests.get(url)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text,"html.parser")
-#ingredients = soup.find_all("ul",{"class:","wprm-recipe-ingredients"}) # stored as an array
-#elems = soup.select("#wprm-recipe-container > div > div.wprm-recipe-ingredients-container > div > ul") 
-#print(ingredients) # will print all of the ingredients sub divided into its seperate  <li> components 
-foods = soup.find_all("span",{"class:", "wprm-recipe-ingredient-name"}) #gets the ingredient names 
-quantity = soup.find_all("span",{"class:","wprm-recipe-ingredient-amount"})
-unit = soup.find_all("span",{"class","wprm-recipe-ingredient-unit"})
 
-print("\n"+soup.title.text+"\n")
-print("Ingredients:")
-
-
+website = url.split("/")[2] # website is the name of the website ex: www.budgetbytes.com
+print(website)
 
 ingredients = ""
-for x in range(len(unit)):
-    #print(quantity[x].text,unit[x].text,foods[x].text)
-    ingredients = ingredients + quantity[x].text +" " +unit[x].text + " " + foods[x].text + "\n"
-
-
-instruction_list = soup.find_all("div",{"class:","wprm-recipe-instruction-text"})
-
 instructions = ""
-for x in range(len(instruction_list)):
-    instructions = instructions + str(x + 1) + ": " + instruction_list[x].text + "\n"
+
+if(website == "www.budgetbytes.com"):
+    #ingredients = soup.find_all("ul",{"class:","wprm-recipe-ingredients"}) # stored as an array
+    #elems = soup.select("#wprm-recipe-container > div > div.wprm-recipe-ingredients-container > div > ul") 
+    #print(ingredients) # will print all of the ingredients sub divided into its seperate  <li> components 
+    foods = soup.find_all("span",{"class:", "wprm-recipe-ingredient-name"}) #gets the ingredient names 
+    quantity = soup.find_all("span",{"class:","wprm-recipe-ingredient-amount"})
+    unit = soup.find_all("span",{"class","wprm-recipe-ingredient-unit"})
+
+
+    print("\n"+soup.title.text+"\n")
+    print("Ingredients:")
+
+
+    for x in range(len(unit)):
+        ingredients = ingredients + quantity[x].text +" " +unit[x].text + " " + foods[x].text + "\n"
+
+    instruction_list = soup.find_all("div",{"class:","wprm-recipe-instruction-text"})
+
+    for x in range(len(instruction_list)):
+        instructions = instructions + str(x + 1) + ": " + instruction_list[x].text + "\n"
+
+elif(website == "www.thecookingguy.com"):
+    recipe_div = soup.find("div",{"class:","sqs-block html-block sqs-block-html"})
+    #print(recipe_div)
+    for ingredients_list in recipe_div:
+        ingredients_list = recipe_div.find("ul")
+        for ingredients_list_entry in ingredients_list:
+            ingredients_list_entry = ingredients_list.find_all("li")
+    
+
+
+
+
+    for x in range(len(ingredients_list_entry)):
+        print(ingredients_list_entry[x].text)
+        type(ingredients_list_entry[x].text)
+        #print(ingredients_list[x].text.split("",2))
+   # print(foods)
+    #print(len(foods))
+
+
+elif(website == "www.allrecipes.com"):
+    print("test2")
 
 print(ingredients)    
 print(instructions)
